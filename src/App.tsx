b61209f3bfc04e611ccff12b57322cebb9956bce
Oliver Ardizzone <oliver.ardizzone@fuvar.hu>
2022-10-27 13:27:02 +0200

    added fonts, created menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import "./sass/main.scss";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="desktop"></div>;
+  return (
+    <section className="desktop">
+      <nav className="menu-bar">
+        <div className="menu-bar__corner-left"></div>
+        <p className="menu-bar__item">test</p>
+      </nav>
+    </section>
+  );
 }
 
 export default App;
