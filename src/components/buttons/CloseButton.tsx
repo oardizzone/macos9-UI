a8baca14acc6cf7b03679857362825af8cc53823
Oliver Ardizzone <oliver.ardizzone@fuvar.hu>
2022-10-27 18:12:05 +0200

    added close button to window

--- src/components/buttons/CloseButton.tsx
+++ src/components/buttons/CloseButton.tsx
@@ -2,8 +2,8 @@ interface CloseButtonProps {}
 
 export const CloseButton = (props: CloseButtonProps) => {
   return (
-    <section className="window-button">
-      <button></button>
+    <section className="window-button__container">
+      <button className="window-button"></button>
     </section>
   );
 };
