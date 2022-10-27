97240f6e628e3aec28f05c1f439295e2eec2ffd0
Oliver Ardizzone <oliver.ardizzone@fuvar.hu>
2022-10-27 21:42:17 +0200

    added styles for article content

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,11 @@ import { useRef, useState } from "react";
 import { MenuBar, Window } from "./components";
 import "./styles/main.scss";
 
+interface WindowData {
+  name: string;
+  wasMoved: boolean;
+}
+
 function App() {
   const desktopRef = useRef<HTMLElement>(null);
   const [openWindows, setOpenWindows] = useState<string[]>([]);
@@ -31,7 +36,27 @@ function App() {
               closeWindow("about");
             }}
           >
-            <h2 className="content__header">About this site</h2>
+            <article className="content">
+              <h2 className="content__header">About this site</h2>
+              <p className="content__text">
+                Here are some words about this site:
+              </p>
+              <p className="content__text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse,
+                illo iusto soluta autem enim repellat aliquid adipisci veritatis
+                iste nihil laboriosam natus. Lorem ipsum dolor sit amet
+                consectetur adipisicing elit. Porro sapiente, autem,
+                necessitatibus reiciendis laudantium eligendi, nemo voluptas
+                nulla fuga velit quas quae? Aliquam eos magnam veniam a quo
+                tempora consequuntur?
+              </p>
+              <p className="content__text">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+                quo quam saepe libero ea voluptatibus nostrum ipsam rem magni
+                laudantium accusantium placeat ratione eveniet, repellat error
+                quod porro numquam minima.
+              </p>
+            </article>
           </Window>
         )}
       </section>
