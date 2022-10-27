690e05695f768bfc192f8e3cb1a39f3762acaea4
Oliver Ardizzone <oliver.ardizzone@fuvar.hu>
2022-10-27 14:14:08 +0200

    added links to menu bar

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,24 @@ function App() {
         <div className="menu-bar__corner--left">
           <div className="menu-bar__corner__spacer"></div>
         </div>
-        <nav className="menu-bar">
-          <p className="menu-bar__item">test</p>
-        </nav>
+        <section className="menu-bar">
+          <nav className="menu-bar__nav">
+            <p className="menu-bar__nav__item">😁</p>
+            <a href="#" className="menu-bar__nav__item">
+              About
+            </a>
+            <a href="#" className="menu-bar__nav__item">
+              Game
+            </a>
+            <a href="#" className="menu-bar__nav__item">
+              Works
+            </a>
+            <a href="#" className="menu-bar__nav__item">
+              Contact
+            </a>
+          </nav>
+          <section className="menu-bar__widgets"></section>
+        </section>
         <div className="menu-bar__corner--right">
           <div className="menu-bar__corner__spacer"></div>
         </div>
