ef1c40bbdb8836659c6163f97f83f86a023c1e9a
Oliver Ardizzone <oliver.ardizzone@fuvar.hu>
2022-10-27 14:01:27 +0200

    finished styling of menu bar corners

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,17 @@ import "./sass/main.scss";
 function App() {
   return (
     <section className="desktop">
-      <nav className="menu-bar">
-        <div className="menu-bar__corner-left"></div>
-        <p className="menu-bar__item">test</p>
-      </nav>
+      <section className="menu-bar__container">
+        <div className="menu-bar__corner--left">
+          <div className="menu-bar__corner__spacer"></div>
+        </div>
+        <nav className="menu-bar">
+          <p className="menu-bar__item">test</p>
+        </nav>
+        <div className="menu-bar__corner--right">
+          <div className="menu-bar__corner__spacer"></div>
+        </div>
+      </section>
     </section>
   );
 }
